eb10f90fd3a4628d386536bded2f4cdeff0e63a7
SantiB02 <santibrasca02@gmail.com>
2024-04-17 00:34:49 -0300

    archivos iniciales

---
+++ Controllers/TodoItemController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo_Ejercicio_Lab4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+    }
+}
