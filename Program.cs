1bf597398d28064386a5479af95972a9dac15c65
SantiB02 <santibrasca02@gmail.com>
2024-04-17 03:06:35 -0300

    retoques finales en appsettings.json y Program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using ToDo_Ejercicio_Lab4.Data;
+using ToDo_Ejercicio_Lab4.Services.Implementations;
+using ToDo_Ejercicio_Lab4.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<TodoContext>(dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["DB:ConnectionString"]));
+
+#region Injections
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<ITodoItemService, TodoItemService>();
+#endregion
 
 var app = builder.Build();
 
